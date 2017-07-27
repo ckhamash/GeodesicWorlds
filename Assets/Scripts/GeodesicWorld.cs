beac84dbd88890243cc358c0325e327a5f511b04
ckhamash <ckhamash@users.noreply.github.com>
2017-07-26 19:38:59 -0700

    Added basic GUI
    Added GUI to allower user to set the dimensions for the geodesic and a button to generate one

--- Assets/Scripts/GeodesicWorld.cs
+++ Assets/Scripts/GeodesicWorld.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -9,16 +10,35 @@ public class GeodesicWorld : MonoBehaviour {
 	public List<Vertex> tiles;
 	public int hexA, hexB; // defines the geodesic type
 	public float radius;
+	public Text inputDimX, inputDimY;
+
 	private float _edgeErrorValue = .001f;
 	// Use this for initialization
-	void Start() {
+	private void Start() {
+		CreateWorld();
+	}
+	
+	public void CreateWorld() {
+		for (int i = tiles.Count - 1; i >= 0; i--) {
+			GameObject.DestroyImmediate(tiles[i].gameObject);
+		}
+
+		if (inputDimX.text != "" && inputDimY.text != "") {
+			hexA = int.Parse(inputDimX.text);
+			hexB = int.Parse(inputDimY.text);
+		}
 		tiles = GeodesicGenerator.GenerateGeodesic(hexA, hexB, vertPrefab);
 		foreach (Vertex t in tiles) {
 			t.transform.SetParent(transform);
 		}
 	}
-	
+
 	// Update is called once per frame
-	void Update() {
+	private void Update() {
+
+	}
+
+	private void OnGUI() {
+		
 	}
 }
